bd07a3bdc97336b2de244f795baf22d537417f89
ashotbes <ashot.bes@gmail.com>
2019-05-03 19:03:07 +0400

    commit

--- app/Main.hs
+++ app/Main.hs
@@ -10,7 +10,7 @@ module Main where
 
 --import           GHC.List              as L
 --import           Data.Maybe
---import qualified Data.ByteString       as B
+import qualified Data.ByteString       as B
 import qualified Data.List             as L
 import qualified Data.Text             as T
 import qualified Data.Text.IO          as TIO
@@ -41,48 +41,41 @@ main = do
     language <- TIO.getLine
     let preparedLanguage = T.toLower language
         correspondingLangFile = L.find (\langFile -> preparedLanguage == T.pack (takeBaseName langFile)) allLangFiles
-    print correspondingLangFile
-    error ""
-
-      {-
-  let langFile    = currentDir </> "i18n" </> "ru.yaml"
-      langFolder  = currentDir </> "i18n"
-  con <- B.readFile $ langFolder </> langFile
-  let parsedContent = Y.decodeThrow con :: Maybe UserPhrase
-  case parsedContent of
-    Nothing -> die "Sorry,we cant Decode this file!"
-    Just phrase -> do
-      let cityNames = (takeCityNames $ phrase) -- :: [Text]
-          result' = [ let names = T.splitOn (",") twoNames
-                          [nameForHuman, nameForServer] = Data.List.filter (not . T.null) names
-                      in (nameForHuman, nameForServer)
-                    | twoNames <- cityNames
-                    ]
-          (allNamesForHumans, _allNamesForServer) = unzip result'
-      TIO.putStrLn (choosLang $ phrase )
-      fileNames <- listDirectory langFolder
-      print fileNames
-      language <- TIO.getLine
-      if (T.toLower $ language) == "ru"
-        then do
-          TIO.putStrLn (choosDate $ phrase)
-          currentTime <- getCurrentTime
-          dateFromUser <- Prelude.getLine
-          let date = getDateFromUser currentTime dateFromUser phrase
-          case date of
-            Left problemWithDate -> TIO.putStrLn problemWithDate
-            Right correctDate -> do
-              TIO.putStrLn $ (choosCity $ phrase)
-              TIO.putStrLn $ T.intercalate "," allNamesForHumans
-              cityFromUser <- TIO.getLine
-              let city = getCityFromUser cityNames cityFromUser
-                  cityNamesForServer = Data.List.filter (\cityFromUser1 -> cityFromUser1 == (T.intercalate "," allNamesForHumans)) allNamesForHumans
-              case city of
-                Nothing -> reportAboutProblem phrase
-                Just correctCity -> do
-                  response <- askWeather (correctDate, Prelude.tail cityNamesForServer)
-                  print $ (Prelude.tail cityNamesForServer)
-                  let answer = prepareAnswer response correctDate correctCity phrase
-                  TIO.putStrLn answer
-                  else TIO.putStrLn "Please select one of the suggested languages!"
-                  -}
+        con <- B.readFile $ langFolder </> langFile
+        let parsedContent = Y.decodeThrow con :: Maybe UserPhrase
+        case parsedContent of
+          Nothing -> die "Sorry,we cant Decode this file!"
+          Just phrase -> do
+            let cityNames = (takeCityNames $ phrase)
+                result' = [ let names = T.splitOn (",") twoNames
+                            [nameForHuman, nameForServer] = Data.List.filter (not . T.null) names
+                            in (nameForHuman, nameForServer)
+                            | twoNames <- cityNames
+                          ]
+                          (allNamesForHumans, _allNamesForServer) = unzip result'
+                          TIO.putStrLn (choosLang $ phrase )
+                          fileNames <- listDirectory langFolder
+                          print fileNames
+                          language <- TIO.getLine
+                          if (T.toLower $ language) == "ru"
+                            then do
+                              TIO.putStrLn (choosDate $ phrase)
+                              currentTime <- getCurrentTime
+                              dateFromUser <- Prelude.getLine
+                              let date = getDateFromUser currentTime dateFromUser phrase
+                              case date of
+                                Left problemWithDate -> TIO.putStrLn problemWithDate
+                                Right correctDate -> do
+                                  TIO.putStrLn $ (choosCity $ phrase)
+                                  TIO.putStrLn $ T.intercalate "," allNamesForHumans
+                                  cityFromUser <- TIO.getLine
+                                  let city = getCityFromUser cityNames cityFromUser
+                                      cityNamesForServer = Data.List.filter (\cityFromUser1 -> cityFromUser1 == (T.intercalate "," allNamesForHumans)) allNamesForHumans
+                                      case city of
+                                        Nothing -> reportAboutProblem phrase
+                                        Just correctCity -> do
+                                          response <- askWeather (correctDate, Prelude.tail cityNamesForServer)
+                                          print $ (Prelude.tail cityNamesForServer)
+                                          let answer = prepareAnswer response correctDate correctCity phrase
+                                          TIO.putStrLn answer
+                                          else TIO.putStrLn "Please select one of the suggested languages!"
