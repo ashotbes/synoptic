ead76a5bef9fd5a922b62c29d830c13b10a86690
ashotbes <ashot.bes@gmail.com>
2019-05-08 17:27:34 +0400

    commit

--- app/Main.hs
+++ app/Main.hs
@@ -7,10 +7,19 @@ module Main where
 --import           AskWeatherFromServer  (askWeather)
 --import           CheckDateAndCity      (getCityFromUser, getDateFromUser,
 --                                        reportAboutProblem)
+--import           AskWeatherFromServer  (askWeather)
+--import           CheckDateAndCity      (getCityFromUser, getDateFromUser,
+--                                        reportAboutProblem)
 
 --import           GHC.List              as L
 --import           Data.Maybe
-import qualified Data.ByteString       as B
+--import qualified Data.ByteString       as B
+--import           Data.List             (filter, unzip)
+--import           Data.Text             as T
+--import           Data.Text.IO          as TIO
+--import           Data.Time.Clock
+--import qualified Data.Yaml             as Y
+--import qualified Data.ByteString       as B
 import qualified Data.List             as L
 import qualified Data.Text             as T
 import qualified Data.Text.IO          as TIO
@@ -19,6 +28,7 @@ import qualified Data.Text.IO          as TIO
 import           System.Directory
 import           System.Exit
 import           System.FilePath.Posix
+--import           Types.UserPhrases     (UserPhrase (..))
 --import           Types.UserPhrases     (UserPhrase (..))
 
 --import           DecodeYaml
@@ -30,52 +40,29 @@ tupleToList _          = []
 
 main :: IO ()
 main = do
-    currentDirectory <- getCurrentDirectory
-    let langDirectory = currentDirectory </> "i18n"
-    allLangFiles <- listDirectory langDirectory >>= \case
-        [] -> die $ "Sorry, but I cannot continue without language support. " ++
-                    "Please make sure the directory " ++ langDirectory ++ " exists."
-        allLangFiles -> return allLangFiles
-    let pureNamesOfLangs = map takeBaseName allLangFiles
-    putStrLn $ "Please choose your language: " ++ L.intercalate ", " pureNamesOfLangs
-    language <- TIO.getLine
-    let preparedLanguage = T.toLower language
-        correspondingLangFile = L.find (\langFile -> preparedLanguage == T.pack (takeBaseName langFile)) allLangFiles
-        con <- B.readFile $ langFolder </> langFile
-        let parsedContent = Y.decodeThrow con :: Maybe UserPhrase
-        case parsedContent of
-          Nothing -> die "Sorry,we cant Decode this file!"
-          Just phrase -> do
-            let cityNames = (takeCityNames $ phrase)
-                result' = [ let names = T.splitOn (",") twoNames
-                            [nameForHuman, nameForServer] = Data.List.filter (not . T.null) names
-                            in (nameForHuman, nameForServer)
-                            | twoNames <- cityNames
-                          ]
-                          (allNamesForHumans, _allNamesForServer) = unzip result'
-                          TIO.putStrLn (choosLang $ phrase )
-                          fileNames <- listDirectory langFolder
-                          print fileNames
-                          language <- TIO.getLine
-                          if (T.toLower $ language) == "ru"
-                            then do
-                              TIO.putStrLn (choosDate $ phrase)
-                              currentTime <- getCurrentTime
-                              dateFromUser <- Prelude.getLine
-                              let date = getDateFromUser currentTime dateFromUser phrase
-                              case date of
-                                Left problemWithDate -> TIO.putStrLn problemWithDate
-                                Right correctDate -> do
-                                  TIO.putStrLn $ (choosCity $ phrase)
-                                  TIO.putStrLn $ T.intercalate "," allNamesForHumans
-                                  cityFromUser <- TIO.getLine
-                                  let city = getCityFromUser cityNames cityFromUser
-                                      cityNamesForServer = Data.List.filter (\cityFromUser1 -> cityFromUser1 == (T.intercalate "," allNamesForHumans)) allNamesForHumans
-                                      case city of
-                                        Nothing -> reportAboutProblem phrase
-                                        Just correctCity -> do
-                                          response <- askWeather (correctDate, Prelude.tail cityNamesForServer)
-                                          print $ (Prelude.tail cityNamesForServer)
-                                          let answer = prepareAnswer response correctDate correctCity phrase
-                                          TIO.putStrLn answer
-                                          else TIO.putStrLn "Please select one of the suggested languages!"
+  currentDirectory <- getCurrentDirectory
+  let langDirectory = currentDirectory </> "i18n"
+  allLangFiles <- listDirectory langDirectory >>= \case
+      [] -> die $ "Sorry, but I cannot continue without language support. " ++
+                  "Please make sure the directory " ++ langDirectory ++ " exists."
+      allLangFiles -> return allLangFiles
+  let pureNamesOfLangs = Prelude.map takeBaseName allLangFiles
+  Prelude.putStrLn $ "Please choose your language: " ++ L.intercalate ", " pureNamesOfLangs
+  language <- TIO.getLine
+  let preparedLanguage = T.toLower language
+      correspondingLangFile = L.find (\langFile -> preparedLanguage == T.pack (takeBaseName langFile)) allLangFiles >>= \case
+          correspondingLangFile1 -> return correspondingLangFile1
+  print preparedLanguage
+  print correspondingLangFile1
+
+
+
+      {-
+  let langFile    = currentDir </> "i18n" </> "ru.yaml"
+      langFolder  = currentDir </> "i18n"
+  con <- B.readFile $ langFolder </> langFile
+@@ -70,3 +85,4 @@ main = do
+                  let answer = prepareAnswer response correctDate correctCity phrase
+                  TIO.putStrLn answer
+                  else TIO.putStrLn "Please select one of the suggested languages!"
+                  -}
