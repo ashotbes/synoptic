006d94ae4bb8da88ecc7dc26b0b5102eebab2151
ashotbes <ashot.bes@gmail.com>
2019-05-17 13:09:25 +0200

    Дополнительная информация в yaml файлах

--- app/Main.hs
+++ app/Main.hs
@@ -3,13 +3,14 @@
 {-# LANGUAGE OverloadedStrings   #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
-module Main where
+module Main where 
 
-import           AskWeatherFromServer  ( askWeather )
-import           CheckDate             ( checkDateFromUser )
-import           PrepareAnswer         ( prepareAnswer )
-import           Types.UserPhrases     ( UserPhrase (..) )
-
+import           AskWeatherFromServer         ( askWeather )
+import           CheckDate                    ( checkDateFromUser )
+import           PrepareAnswer                ( prepareAnswer )
+import             Types.UserPhrases            ( UserPhrase (..) )
+ 
+import           Network.HTTP.Simple          
 import           Control.Exception            ( IOException, try )
 import qualified Data.ByteString       as B   ( readFile )
 import qualified Data.List             as L   ( find, filter ,lookup, intercalate )
@@ -18,7 +19,7 @@ import qualified Data.Text.IO          as TIO ( putStrLn, getLine )
 import           Data.Time.Clock              ( getCurrentTime )
 import qualified Data.Yaml             as Y   ( decodeThrow )
 import           System.Directory             ( getCurrentDirectory, listDirectory )
-import           System.Exit                  ( die )
+import           System.Exit                  ( die, exitFailure )
 import           System.FilePath.Posix        ( takeBaseName, (</>) )
 
 main :: IO ()
@@ -38,9 +39,10 @@ main = do
     -- Ask user for a language.
     language <- TIO.getLine
     let preparedLanguage = T.toLower language -- User may use different case on his keyboard.
-        Just correspondingLangFile = L.find (\langFile -> preparedLanguage == T.pack (takeBaseName langFile))
-                                            allLangFiles
-        pathToLangFile = langDirectory </> correspondingLangFile
+    correspondingLangFile <- case L.find (\langFile -> preparedLanguage == T.pack (takeBaseName langFile)) allLangFiles of 
+                                     Nothing -> die "You have entered an incorrect or unsupported language, please select one of the languages above!"
+                                     Just correspondingLangFile -> return correspondingLangFile 
+    let pathToLangFile = langDirectory </> correspondingLangFile
     langFileContent <- try (B.readFile pathToLangFile) >>= \case
         Left (someProblem :: IOException) -> die $ "Sorry, I cannot open language file "
                                                    <> pathToLangFile
@@ -50,7 +52,7 @@ main = do
     -- Parse file's content to a structure.
     phrasesForUser <- case Y.decodeThrow langFileContent :: Maybe UserPhrase of
         Nothing             -> die "Sorry, I cannot parse language file."
-        Just phrasesForUser -> return phrasesForUser
+        Just phrasesForUser -> return phrasesForUser  
     -- Extract cities' names (for human and for server).
     let cityNames = cities phrasesForUser
         pairsOfCityNames = [ let names = T.splitOn (",") twoNames
@@ -62,12 +64,12 @@ main = do
     -- Show all supported cities (already in chosen language).
     TIO.putStrLn $ messageChooseForecastCity phrasesForUser
     TIO.putStrLn $ T.intercalate ", " allNamesForHumans
-    cityFromUser <- TIO.getLine
+    cityFromUser <- TIO.getLine 
     -- Find corresponding name of the city, but for the server.
     cityNameForServer <- case L.lookup cityFromUser pairsOfCityNames of
         Nothing -> do
             TIO.putStrLn $ messageErrorWrongCity phrasesForUser
-            die ""
+            exitFailure 
         Just cityNameForServer -> return cityNameForServer
     -- Ask user for a date of forecast.
     TIO.putStrLn $ messageChooseForecastDate phrasesForUser
@@ -75,13 +77,20 @@ main = do
     currentTime <- getCurrentTime
     dateFromUser <- Prelude.getLine
     correctDate <- case checkDateFromUser currentTime dateFromUser phrasesForUser of
-        Left problemWithDate -> TIO.putStrLn problemWithDate >> die ""
+        Left problemWithDate -> TIO.putStrLn problemWithDate >> exitFailure
         Right correctDate    -> return correctDate
     -- Ask a weather from the server.
     responseFromServer <- askWeather cityNameForServer
-    -- Form the final response for the user.
-    let finalAnswer = prepareAnswer responseFromServer
-                                    correctDate
-                                    cityFromUser
-                                    phrasesForUser
-    TIO.putStrLn finalAnswer
+    let someThing = getResponseStatusCode responseFromServer 
+    if someThing /= 200 
+        then TIO.putStrLn $ messageUnexpectedError phrasesForUser 
+        else do 
+            let finalAnswer = prepareAnswer responseFromServer
+                                             correctDate
+                                             cityFromUser
+                                             phrasesForUser 
+            TIO.putStrLn finalAnswer
+
+ -- Form the final response for the user.
+
+
--- src/AskWeatherFromServer.hs
+++ src/AskWeatherFromServer.hs
@@ -3,15 +3,15 @@ module AskWeatherFromServer where
 import           Data.ByteString.Lazy ( ByteString )
 import           Data.Text            ( Text, unpack )
 import           Network.HTTP.Client  ( Response, defaultManagerSettings,
-                                        httpLbs, newManager, parseRequest,
-                                        responseBody )
+                                        httpLbs, newManager, responseBody
+                                      , parseUrlThrow )
 
 -- Отправляем запрос на сервер и получаем ответ
 
 askWeather :: Text -> IO (Response ByteString)
 askWeather cityFromUser = do
     manager <- newManager defaultManagerSettings
-    request <- parseRequest $ "http://api.openweathermap.org/data/2.5/forecast?q="
+    request <- parseUrlThrow $ "http://api.openweathermap.org/data/2.5/forecast?q="
                               <> unpack cityFromUser
                               <> ",am&mode=json&appid=9e23b1eea9c1da6e75d81b6271c0379c"
     httpLbs request manager
